a44dab38d357ad6bdfffbcd125ac54984cf23587
Korczcz <kapiwlkp33@gmail.com>
2023-05-30 11:08:23 +0200

    added account change, deletion form

--- konto.php
+++ konto.php
@@ -15,6 +15,15 @@ if (!isset($_SESSION['zalogowane_id'])){
     <title>Document</title>
 </head>
 <body>
+  <?php
+    require_once("database.php");
+
+    $klinetKwerenta=$db->prepare(
+      'SELECT `klientNick`, `klientImie`, `klientNazwisko`, `klientMail`, `klientHaslo` FROM `klient` WHERE `klientID` = :vid');
+    $klinetKwerenta->bindValue('vid', $_SESSION['zalogowane_id'], PDO::PARAM_STR);
+    $klinetKwerenta->execute();
+    $dane = $klinetKwerenta->fetch();
+  ?>
     <header>
     <nav>
       <ul>
@@ -29,23 +38,30 @@ if (!isset($_SESSION['zalogowane_id'])){
     </nav>
     </header>
     <h1>Zmień informacje</h1>
-    <form action="rejestracja.php" method="post">
+    <form action="zmienInformacje.php" method="post">
+        <label for="name"> Imie:</label>
+        <input type="text" name="name" value=<?php echo '"' .$dane['klientImie']. '"';?> required><br>
+
+        <label for="surname"> Nazwisko:</label>
+        <input type="text" name="surname" value=<?php echo '"' .$dane['klientNazwisko']. '"';?> required><br>
+
+        <label for="mail"> Mail:</label>
+        <input type="mail" name="mail" value=<?php echo '"' .$dane['klientMail']. '"';?> required <?= (isset($_SESSION['wprowadzony_mail']))?'value="' .$_SESSION['wprowadzony_mail'].'"' : '';?>><br>
+
+        <input type="submit" value="Zmień dane">
+    </form>
+    <h1>Zmień login lub hasło</h1>
+    <form action="zmienLogin.php" method="post">
         <label for="log"> Login:</label>
-        <input type="text" name="log" required><br>
+        <input type="text" name="log" value="<?php echo $dane['klientNick'];?>" required><br>
 
         <label for="pass"> Hasło:</label>
         <input type="password" name="pass" required><br>
-
-        <label for="name"> Imie:</label>
-        <input type="text" name="name" required><br>
-
-        <label for="surname"> Nazwisko:</label>
-        <input type="text" name="surname" required><br>
-
-        <label for="mail"> Mail:</label>
-        <input type="mail" name="mail" required <?= (isset($_SESSION['wprowadzony_mail']))?'value="' .$_SESSION['wprowadzony_mail'].'"' : '';?>><br>
-
         <input type="submit" value="Zmień dane">
+    </form>
+    <h1>Usuń konto</h1>
+    <form action="usunKonto.php" method="post">
+        <input type="submit" value="Usuń konto">
     </form>
     <?php
         if (isset($_SESSION['wprowadzony_mail'])){
